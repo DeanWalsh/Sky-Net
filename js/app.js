281f6ce552ff239229c0a1a23c48e07c120b8cc5
DeanWalsh <deanochambers@gmail.com>
2021-02-14 13:40:47 +0000

    location api added

--- js/app.js
+++ js/app.js
@@ -2,28 +2,33 @@ window.addEventListener("load", () => {
     let long;
     let lat;
     let air_temps = document.querySelector(".air_temp");
+    let location = document.querySelector(".location");
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            const api =  `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`;
+            var weatherApi =  `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`;
+            var locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`;
+
+            fetch(locationApi)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const {city} = data;
+                location.textContent = city;
+            });
             
-            fetch(api)
+            fetch(weatherApi)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 const {air_temperature} = data.properties.timeseries[0].data.instant.details;
-                console.log(data);
-                console.log(air_temperature);
-
                 air_temps.textContent = air_temperature;
-
             });
         });
     }
-
-
-
+    
 });
